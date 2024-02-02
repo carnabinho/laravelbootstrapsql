d6db6511860a592d202ef0eec9efc2aeba9a616c
GABRIEL DE OLIVEIRA CARNAVAL <gabriel.ocarnaval@senacsp.edu.br>
2024-02-02 17:10:56 -0300

    primeiro commit

---
+++ resources/views/paginacadastro.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<body>
+<style>body{background-color:#0E1322;}</style>
+<div class="container text-center">
+  <div class="row">
+    <div class="col">
+    <div class="container text-center">
+  <div class="row">
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+  </div>
+</div>
+<div class="col">
+    <h1 style= color:white>CADASTRO</h1>
+    <p style= color:white>Insira seus dados para se cadastrar!</p>    
+    </div>
+    <BR><BR><Br></Br></BR></BR>
+    <div class="card" style="width: 18rem;">
+  <div class="card-body">
+  <div class="mb-3">
+  <label for="formGroupExampleInput" class="form-label"></label>
+  <input type="text" class="form-control" id="formGroupExampleInput" placeholder="CPF">
+</div>
+<div class="mb-3">
+  <label for="formGroupExampleInput2" class="form-label"></label>
+  <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="NOME">
+</div>
+<div class="mb-3">
+  <label for="formGroupExampleInput3" class="form-label"></label>
+  <input type="text" class="form-control" id="formGroupExampleInput3" placeholder="TELEFONE">
+</div>
+<div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label"></label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="EMAIL">
+</div>
+<div class="mb-3">
+<label for="exampleInputEmail1" class="form-label"></label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="CONFIRMAR EMAIL">
+</div>
+<div class="mb-3">
+<div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label"></label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="SENHA">
+</div>
+<div class="mb-3">
+<label for="exampleInputPassword1" class="form-label"></label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="CONFIRMER SENHA">
+</div>
+  </div>
+  </div>
+    </div>
+    
+    <a href="login">CRIAR CONTA</a>
+    </div>
+  </div>
+</div>
+
+    
+</body>
+</html>
---
+++ resources/views/paginalogin.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    
+</head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<body>
+<style>body{background-color:#0E1322;}</style>
+<div class="container text-center">
+  <div class="row">
+    <div class="col">
+      
+    </div>
+    <div class="col"><br><br><br><br><br><br><br>
+    <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <style>card-body{background-color: #252936}</style>
+  <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">Nós nunca compartilharemos seu email com ninguém.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Senha</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Lembre-se de mim</label>
+    <br>
+    <p>não tem uma conta? <a class="link-opacity-100" href="cadastro">Inscreva-se</a></p>
+  </div>
+  <button type="submit" class="btn btn-primary">Entrar</button>
+</form>
+  </div>
+</div>
+    </div>
+    <div class="col">
+      
+    </div>
+  </div>
+</div>
+</body>
+</html>
